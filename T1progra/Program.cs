251ea57ed27b45c183d157e5a97d13b29bc77ad5
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-15 17:59:29 -0600

    si

--- T1progra/Program.cs
+++ T1progra/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Bievenido al primer programa con muchos datos\n\n");
 
 Console.WriteLine("Primera series\n 2. Operaciones con tipos de datos \n \n ");
-
+Console.WriteLine("Mario se la come \n \n ");
