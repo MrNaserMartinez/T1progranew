460d3f04d890fdd371637ed504876836326c5c57
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-15 17:15:49 -0600

    si

--- T1progra/Program.cs
+++ T1progra/Program.cs
@@ -1,21 +1,4 @@
 ﻿Console.WriteLine("Bievenido al primer programa con muchos datos\n\n");
 
-Console.WriteLine("Primera series\n 1. Conversión de tipo\n \n ");
+Console.WriteLine("Primera series\n 2. Operaciones con tipos de datos \n \n ");
 
-Console.WriteLine("*Conversión de número entero a decimal:");
-Console.WriteLine("Ingresa el número para el proceso\n" );
-int n1 = Convert.ToInt32(Console.ReadLine());
-decimal n2 = Convert.ToDecimal(n1);
-Console.WriteLine($"El número decimal es: {n2}\n");
-
-Console.WriteLine("*Conversión de número decimal a flotante:");
-Console.WriteLine("Ingresa el número para el proceso\n");
-decimal n3 = Convert.ToDecimal(Console.ReadLine());
-float n4 = Convert.ToSingle(n3);
-Console.WriteLine($"El número flotante es: {n4}\n");
-
-Console.WriteLine("*Conversión de número flotante a entero:");
-Console.WriteLine("Ingresa el número para el proceso\n");
-decimal n5 = Convert.ToDecimal(Console.ReadLine());
-float n6 = Convert.ToSingle(n5);
-Console.WriteLine($"El número flotante es: {n6}\n");
