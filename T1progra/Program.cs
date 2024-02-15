f42f428a8df3cc99864bca433ce3d8b3903e9ed4
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-15 16:10:51 -0600

    si

--- T1progra/Program.cs
+++ T1progra/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Primera series\n 1. Conversión de tipo\n \n ");
 
 Console.WriteLine("*Conversión de número entero a decimal:");
-Console.WriteLine("Ingresa el número para el proceso\n );
+Console.WriteLine("Ingresa el número para el proceso\n" );
 int n1 = Convert.ToInt32(Console.ReadLine());
 decimal n2 = Convert.ToDecimal(n1);
-Console.WriteLine($"El número decimal es: {n2}");
+Console.WriteLine($"El número decimal es: {n2}\n");
