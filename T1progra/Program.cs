5142ca0e89fd69f68779281258dcac832f891ff4
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-15 16:09:11 -0600

    nuevo

--- T1progra/Program.cs
+++ T1progra/Program.cs
@@ -1,14 +1,9 @@
-﻿
-Console.WriteLine("Bievenido al primer programa con muchos datos feliz feliz\n\n");
+﻿Console.WriteLine("Bievenido al primer programa con muchos datos feliz feliz\n\n");
 
 Console.WriteLine("Primera series\n 1. Conversión de tipo\n \n ");
 
 Console.WriteLine("*Conversión de número entero a decimal:");
-int numeroEntero = 10;
-decimal numeroDecimal = Convert.ToDecimal(numeroEntero);
-Console.WriteLine($"El número entero {numeroEntero} convertido a decimal es: {numeroDecimal}");
-
-Console.WriteLine("\n*Conversión de número decimal a flotante:");
-decimal numeroDecimal2 = 10.5m;
-float numeroFlotante = Convert.ToSingle(numeroDecimal2);
-Console.WriteLine($"El número decimal {numeroDecimal2} convertido a flotante es: {numeroFlotante}");
+Console.WriteLine("Ingresa el número para el proceso\n );
+int n1 = Convert.ToInt32(Console.ReadLine());
+decimal n2 = Convert.ToDecimal(n1);
+Console.WriteLine($"El número decimal es: {n2}");
