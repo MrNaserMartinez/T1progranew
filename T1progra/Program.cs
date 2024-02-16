9755b5fb96ded858944f122d37913a22c06582cd
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-15 20:52:42 -0600

    si

--- T1progra/Program.cs
+++ T1progra/Program.cs
@@ -2,7 +2,10 @@
 
 Console.WriteLine("Bievenido al primer programa con muchos datos\n\n");
 
-Console.WriteLine("Segundo serie\n 2. Uso de Char y String\n ");
+Console.WriteLine("Tercera serie\n 3. Uso de Char y String\n ");
 
-
+Console.WriteLine("*Ingrese una cadena de texto:");
+string text = Console.ReadLine();
+string Mayus = text.ToUpper();
+Console.WriteLine($"La cadena en mayúsculas es: {Mayus}");
 
