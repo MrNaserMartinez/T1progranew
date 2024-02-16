3a6e227e0acb6ff147c4f6754f54443f23ff2ef1
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-15 20:10:02 -0600

    new2

--- T1progra/Program.cs
+++ T1progra/Program.cs
@@ -2,10 +2,19 @@
 
 Console.WriteLine("Segundo serie\n 2. Operaciones con tipos de datos\n ");
 
-Console.WriteLine("Conversión de dos números enteros\n");
+Console.WriteLine("Suma de dos números enteros\n");
 Console.WriteLine("Ingrese el primer número entero:");
 int n1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Ingrese el segundo número entero:");
 int n2 = Convert.ToInt32(Console.ReadLine());
 int suma = n1 + n2;
 Console.WriteLine($"La suma de {n1} y {n2} es: {suma}");
+
+Console.WriteLine("Resta de dos número decimales\n");
+Console.WriteLine("Ingrese el primer número decimal:");
+decimal n3 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo número decimal:");
+decimal n4 = Convert.ToInt32(Console.ReadLine());
+decimal resta = n3 - n4;
+Console.WriteLine($"La suma de {n3} y {n4} es: {resta}");
+
