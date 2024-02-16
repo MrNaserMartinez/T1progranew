e1cc28b51eb9d8a95940600b8efc54642a2e8bcc
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-15 20:05:02 -0600

    new1

--- T1progra/Program.cs
+++ T1progra/Program.cs
@@ -1,3 +1,11 @@
 ﻿Console.WriteLine("Bievenido al primer programa con muchos datos\n\n");
 
-Console.WriteLine("Segundo serie\n 2. Operaciones con tipos de datos \n \n ");
+Console.WriteLine("Segundo serie\n 2. Operaciones con tipos de datos\n ");
+
+Console.WriteLine("Conversión de dos números enteros\n");
+Console.WriteLine("Ingrese el primer número entero:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo número entero:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int suma = n1 + n2;
+Console.WriteLine($"La suma de {n1} y {n2} es: {suma}");
